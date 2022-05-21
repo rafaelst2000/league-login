59c4336ff304b15eb8c457ad78019426e436c362
rafaelst2000 <rafaelst2000@gmail.com>
2022-05-21 19:28:11 -0300

    ✨ creating info banner

---
+++ src/components/LoggedScreenInfo.vue
@@ -0,0 +1,120 @@
+<script>
+/* import LoggedScreenContainer from '../components/LoggedScreenContainer.vue' */
+
+export default {
+  components: {
+    /*  LoggedScreenContainer, */
+  },
+  data: () => ({}),
+}
+</script>
+
+<template>
+  <div class="logged-info-container">
+    <h1>Sobre</h1>
+
+    <div class="center">
+      <p>
+        Mostre suas habilidades em League of Legends: o MOBA 5v5 que combina sinergia de equipe e maestria individual. Escolha sua função, otimize sua itemização e abra caminho pelo mapa para destruir o Nexus inimigo. Com mais de 150 Campeões e inúmeras maneiras de jogar, encontre o Campeão perfeito
+        para você e forje seu caminho rumo à vitória!
+      </p>
+      <h6>DISPONÍVEL TAMBÉM PARA</h6>
+      <div class="apple">
+        <i class="fa-brands fa-apple"></i>
+        <h5>MacOS</h5>
+      </div>
+    </div>
+
+    <div class="riot-games">
+      <span>br.leagueoflegends.com</span>
+
+      <h6>DESENVOLVEDORA</h6>
+      <h5>Riot Games</h5>
+
+      <h6>DISTRIBUIDORA</h6>
+      <h5>Riot Games</h5>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.logged-info-container {
+  position: relative;
+  width: 1280px;
+  height: 680px;
+  background: #141212;
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  justify-items: center;
+  gap: 40px;
+  padding: 50px 30px 0 0;
+
+  h1 {
+    color: #f9f9f9;
+  }
+
+  & .center {
+    p {
+      color: #b8b8b8;
+      font-size: 20px;
+      line-height: 1.5;
+    }
+
+    h6 {
+      color: #f9f9f9;
+      margin: 50px 0 25px;
+      font-size: 14px;
+    }
+
+    .apple {
+      width: 140px;
+      border-radius: 8px;
+      padding: 20px;
+      background: #201e1e;
+      color: #f9f9f9;
+      display: flex;
+      gap: 10px;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      transition: 0.3s;
+      & i {
+        color: #a2a1a1;
+        font-size: 28px;
+        transition: 0.3s;
+      }
+
+      & h5 {
+        font-size: 18px;
+      }
+
+      &:hover {
+        i {
+          color: #f9f9f9;
+        }
+      }
+    }
+  }
+
+  & .riot-games {
+    & span {
+      color: #f9f9f9;
+      font-weight: bold;
+      display: inline-block;
+      margin-bottom: 30px;
+    }
+
+    & h6 {
+      color: #525252;
+      font-size: 14px;
+      margin-bottom: 5px;
+    }
+
+    & h5 {
+      color: #b8b8b8;
+      font-size: 16px;
+      margin-bottom: 30px;
+    }
+  }
+}
+</style>
--- src/views/LoggedScreen.vue
+++ src/views/LoggedScreen.vue
@@ -1,267 +1,41 @@
 <script>
-import GameAlert from '../components/GameAlert.vue'
-import OutlinedButton from '../components/OutlinedButton.vue'
+import LoggedScreenBanner from '../components/LoggedScreenBanner.vue'
+import LoggedScreenInfo from '../components/LoggedScreenInfo.vue'
+
 export default {
   components: {
-    GameAlert,
-    OutlinedButton,
+    LoggedScreenBanner,
+    LoggedScreenInfo,
   },
-  data: () => ({
-    isVideo: false,
-  }),
-  computed: {
-    cameraIcon() {
-      return this.isVideo ? 'fas fa-video' : 'fas fa-video-slash'
-    },
-  },
+  data: () => ({}),
 }
 </script>
 
 <template>
-  <div class="logged-container-outside">
-    <video autoplay muted loop id="myVideo" v-if="isVideo">
-      <source src="../assets/valiant-riven.mp4" type="video/mp4" />
-    </video>
-    <div class="logged-container" :class="{ 'background-image': !isVideo }">
-      <header>
-        <div class="left-icons">
-          <outlined-button icon="fas fa-border-all" />
-          <game-alert />
-        </div>
-        <img src="../assets/league-of-legends.png" alt="" />
-        <outlined-button icon="fas fa-user" />
-      </header>
-      <footer :class="isVideo ? 'footer-video' : 'footer-image'">
-        <outlined-button class="icon-camera" :icon="cameraIcon" @click="isVideo = !isVideo" />
-        <div class="play-button-container">
-          <div class="play-button">
-            <div class="play-circle"><i class="fas fa-play"></i></div>
-            <h1>Jogar</h1>
-          </div>
-
-          <div class="button-dropdown">
-            <i class="fa-solid fa-caret-down"></i>
-          </div>
-        </div>
-
-        <div class="footer-cards">
-          <div class="card card-1">
-            <div>
-              <h6>DEV</h6>
-            </div>
-            <h5>
-              Mapa dos Campeões - Abril <br />
-              de 2022
-            </h5>
-          </div>
-          <div class="card card-2">
-            <div>
-              <h6>ATUALIZAÇÃO DO JOGO</h6>
-            </div>
-            <h5>Notas da Atualização 12.9</h5>
-          </div>
-        </div>
-      </footer>
-    </div>
+  <div class="logged-container">
+    <logged-screen-banner />
+    <logged-screen-info />
   </div>
 </template>
 
 <style lang="scss" scoped>
-#myVideo {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  width: 1280px;
-  height: 680px;
-  object-fit: cover;
-  animation: fadeIn 1.5s;
-}
-
-.logged-container-outside {
-  position: relative;
-  width: 1280px;
-  height: 680px;
-}
-
-.background-image {
-  background-image: url('../assets/valiant-riven.jpg');
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  animation: fadeIn 1.5s;
-}
-
 .logged-container {
-  position: absolute;
-  width: 1280px;
   height: 680px;
-
-  & header {
-    height: 100px;
-    width: 100%;
-    padding: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    & .left-icons {
-      display: flex;
-      align-items: center;
-      gap: 20px;
-    }
-    img {
-      display: block;
-      width: 150px;
-    }
-  }
-
-  .footer-video {
-    background: rgba(0, 0, 0, 0.5);
-  }
-  .footer-image {
-    background: rgba(0, 0, 0, 0.3);
-  }
-
-  & footer {
-    height: 120px;
-    width: 100%;
-    padding: 20px 80px;
-    position: absolute;
-    bottom: 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  & .icon-camera {
-    position: absolute;
-    bottom: 140px;
-    left: 20px;
-  }
-
-  & .play-button-container {
-    width: 240px;
-    height: 70px;
-    display: flex;
-    align-items: center;
-    gap: 2px;
-    cursor: pointer;
-    transition: 0.3s;
-    &:hover {
-      .play-circle {
-        transform: scale(1.1);
-        font-size: 22px;
-      }
-    }
-  }
-
-  & .button-dropdown {
-    height: 100%;
-    width: calc(240px - 190px);
-    background: rgba(#1d454b, 0.9);
-    color: #a1b1b3;
-    font-size: 22px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-  }
-
-  & .play-button {
-    box-shadow: rgba(#14b4d8, 0.4) 0px 0px 30px 4px;
-    width: 190px;
-    height: 100%;
-    background: rgb(71, 159, 203);
-    background: linear-gradient(148deg, rgba(71, 159, 203, 1) 35%, rgba(12, 194, 225, 1) 100%);
-    border-top-left-radius: 20px;
-    border-bottom-left-radius: 20px;
-    border-top-right-radius: 4px;
-    border-bottom-right-radius: 4px;
-    color: #f9f9f9;
-    display: flex;
-    gap: 15px;
-    align-items: center;
-    padding-left: 15px;
-    transition: 0.3s;
-
-    &:hover {
-      box-shadow: rgba(#14b4d8, 0.8) 0px 0px 30px 4px;
-    }
-
-    & h1 {
-      line-height: 1;
-    }
-    & .play-circle {
-      background: red;
-      width: 45px;
-      height: 45px;
-      border-radius: 50%;
-      background: #141212;
-      color: #f9f9f9;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      transition: 0.3s;
-      & i {
-        font-size: 18px;
-      }
-    }
-  }
-
-  & .footer-cards {
-    margin-bottom: 90px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    gap: 20px;
-    & h6 {
-      color: #1e1a24;
-      background: rgba(#f9f9f9, 0.5);
-      border-radius: 8px;
-      padding: 4px 8px;
-      display: inline-block;
-    }
-    & h5 {
-      color: #efefef;
-      font-size: 14px;
-    }
-    & .card {
-      padding: 10px;
-      display: flex;
-      justify-content: space-between;
-      flex-direction: column;
-      height: 130px;
-      border-radius: 8px;
-      background-size: cover;
-      background-position: center;
-      background-repeat: no-repeat;
-      transition: 0.3s;
-      cursor: pointer;
-
-      &:hover {
-        border: 4px solid #f9f9f9;
-        border-radius: 8px;
-      }
-    }
-
-    & .card-1 {
-      background-image: url('../assets/mapa-campeoes.jpg');
-      width: 230px;
-    }
-    & .card-2 {
-      background-image: url('../assets/notas-atualizacao.jpg');
-      width: 320px;
-    }
-  }
+  overflow: auto;
+  width: 1288px;
 }
 
-@keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
+::-webkit-scrollbar {
+  width: 8px; /* width of the entire scrollbar */
+}
+
+::-webkit-scrollbar-track {
+  background: rgba(#000000, 0.4);
+  border-radius: 8px;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: rgba(#f9f9f9, 0.9);
+  border-radius: 10px;
 }
 </style>
