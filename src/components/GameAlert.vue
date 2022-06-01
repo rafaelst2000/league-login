21d07cd01a91dd922beaea809162a3e261a6698b
rafaelst2000 <rafaelst2000@gmail.com>
2022-06-01 09:35:20 -0300

    ✨ add game alert tooltip

--- src/components/GameAlert.vue
+++ src/components/GameAlert.vue
@@ -1,10 +1,34 @@
-<script></script>
-
+<script>
+export default {
+  data: () => ({
+    showTooltip: false,
+  }),
+}
+</script>
 <template>
-  <div class="game-alert"></div>
+  <span class="game-alert-out">
+    <div class="game-alert" @mouseenter="showTooltip = true" @mousedown="showTooltip = false"></div>
+    <div class="game-alert-tooltip" v-if="showTooltip" @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
+      <div class="item">
+        <h3>Skins desabilitadas</h3>
+        <span><i class="fas fa-clock"></i> 26/05/2022 18:47 </span>
+        <p>Desabilitamos temporariamente as skins a seguir enquanto trabalhamos em uma solução para o conflito: Kayn Odisseia</p>
+      </div>
+      <div class="item">
+        <h3>Presentes</h3>
+        <span><i class="fas fa-clock"></i> 12/05/2021 19:03 </span>
+        <p>Presentes de conteúdo (campeões, skins, etc) estão temporariamente indisponíveis enquanto trabalhamos na solução de um problema. Presentes de RP podem ser enviados normalmente.</p>
+      </div>
+
+      <h6>MAIS DETALHES</h6>
+    </div>
+  </span>
 </template>
 
 <style lang="scss" scoped>
+.game-alert-out {
+  position: relative;
+}
 .game-alert {
   width: 34px;
   height: 34px;
@@ -12,6 +36,7 @@
   transform: rotate(45deg);
   position: relative;
   border-radius: 4px;
+  transition: 0.3s;
   &::after {
     content: '!';
     font-size: 32px;
@@ -21,6 +46,51 @@
     position: absolute;
     top: -2px;
     left: 11px;
+  }
+  &:hover {
+    background: #a743b0;
+  }
+}
+.game-alert-tooltip {
+  background: #f9f9f9;
+  padding: 30px;
+  width: 400px;
+  position: absolute;
+  left: 50px;
+  top: 0;
+  border-radius: 4px;
+
+  & h6 {
+    color: #b6b6b6;
+    cursor: pointer;
+    transition: 0.3s;
+    &:hover {
+      color: #111;
+    }
+  }
+
+  & .item {
+    margin-bottom: 30px;
+    h3 {
+      color: #111;
+      font-size: 16px;
+      margin-bottom: 10px;
+    }
+    span {
+      color: #999;
+      font-size: 14px;
+      font-weight: bold;
+      & i {
+        font-size: 12px;
+        margin-right: 3px;
+      }
+    }
+    p {
+      margin-top: 5px;
+      color: #111;
+      font-size: 14px;
+      line-height: 1;
+    }
   }
 }
 </style>
